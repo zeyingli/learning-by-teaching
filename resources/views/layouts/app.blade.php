4434bdeb1f31024163e1e74e1c91e837e255acc2
Zeying Li <admin@zeyingli.com>
2018-03-21 15:24:38 -0400

    updated favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,10 @@
 
         <title>@if (trim($__env->yieldContent('template_title')))@yield('template_title') | @endif {{ config('app.name', Lang::get('titles.app')) }}</title>
         <meta name="description" content="">
-        <meta name="author" content="Jeremy Kenedy">
-        <link rel="shortcut icon" href="/favicon.ico">
+        <meta name="author" content="Zeying Li">
+
+        {{-- Favicon --}}
+        <link rel="shortcut icon" href="../favicon.png">
 
         {{-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries --}}
         <!--[if lt IE 9]>
