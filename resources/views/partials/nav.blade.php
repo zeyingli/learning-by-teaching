77765952747eb7d0c3d5534241bfe6a311869e95
Zeying Li <admin@zeyingli.com>
2018-03-21 15:25:30 -0400

    added entrances for each page

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -19,6 +19,19 @@
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             {{-- Left Side Of Navbar --}}
             <ul class="nav navbar-nav">
+                @if (Auth::guest())
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        Activity <span class="caret"></span>
+                    </a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li {{ Request::is('start', 'start/') ? 'class=active' : null }}>{!! HTML::link(url('/start'), 'Start') !!}</li>
+                            <li {{ Request::is('overview', 'overview/') ? 'class=active' : null }}>{!! HTML::link(url('/overview'), 'Overview') !!}</li>
+                            <li {{ Request::is('participant', 'participant/') ? 'class=active' : null }}>{!! HTML::link(url('/participant'), 'Participant List') !!}</li>
+                        </ul>
+                </li>
+                @endif
+
                 @role('admin')
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -41,8 +54,8 @@
             <ul class="nav navbar-nav navbar-right">
                 {{-- Authentication Links --}}
                 @if (Auth::guest())
-                    <li><a href="{{ route('login') }}">{!! trans('titles.login') !!}</a></li>
                     @if (env('ALLOW_USER_REGISTRATION'))
+                    <li><a href="{{ route('login') }}">{!! trans('titles.login') !!}</a></li>
                     <li><a href="{{ route('register') }}">{!! trans('titles.register') !!}</a></li>
                     @endif
                 @else
